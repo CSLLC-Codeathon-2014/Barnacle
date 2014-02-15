19d7c1f1d2aeef47438e6706c3826117b9ab3208
thisisdaleb <officialgsproductions@gmail.com>
2014-02-15 14:28:24 -0600

    First Physics Check
    Initial Physics and Collision. "Play Now" as character.

--- src/Map1.java
+++ src/Map1.java
@@ -4,6 +4,11 @@ import org.newdawn.slick.state.*;
 public class Map1 extends BasicGameState {
 	Image land;
 	private int state;
+	Image chicken;
+	int chickX=450;
+	int chickY=400;
+	int VelX=0;
+	int VelY=0;
 
 	public Map1(int state) {
 		 this.state = state;
@@ -13,18 +18,54 @@ public class Map1 extends BasicGameState {
 	public void init(GameContainer gc, StateBasedGame sbg)
 			throws SlickException {
 		land = new Image("bg.png");
+		chicken= new Image("playNow.png");
 	}
 
 	@Override
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g)
 			throws SlickException {
 		land.draw(0, 0, gc.getWidth(), gc.getHeight());
+		chicken.draw(chickX, chickY, gc.getWidth()/18, gc.getHeight()/32);
 	}
 
 	@Override
 	public void update(GameContainer gc, StateBasedGame sbg, int delta)
 			throws SlickException {
+		Input input = gc.getInput();
+		if(chickY>590 && chickY<635 && chickX>350 && chickX<1250){
+			chickY=600;
+			VelY=0;
+		}
+		if(input.isKeyDown(Input.KEY_SPACE)){
+			if(VelY<5)
+			VelY=20;
+		}
+		VelY--;
+		if(VelY>20)
+		VelY=20;
+		if(VelY<-20)
+			VelY=-20;
+		if(input.isKeyDown(Input.KEY_A)){
+		if(VelX>1)
+			VelX=1;
+		VelX-=2;
+		}
+		if(input.isKeyDown(Input.KEY_D)){
+			if(VelX<-1)
+				VelX=-1;
+			VelX+=2;
+			}
+		if(VelX<-20)
+			VelX=-20;
+			if(VelX>20)
+				VelX=20;
+		if(VelX>0)
+		VelX--;
+		if(VelX<0)
+		VelX++;
 		
+		chickY=chickY-VelY;
+		chickX=chickX+VelX;
 	}
 
 	@Override
