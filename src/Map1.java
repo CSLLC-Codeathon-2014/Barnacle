855b01a4e3ff3fe18aa7d530ab1def33800ff919
thisisdaleb <officialgsproductions@gmail.com>
2014-02-15 17:55:28 -0600

    Add multiplayer.

--- src/Map1.java
+++ src/Map1.java
@@ -3,18 +3,31 @@ import org.newdawn.slick.state.*;
 
 public class Map1 extends BasicGameState {
 	private int state;
+	/*private boolean playCollider1 = false;
+	private boolean playCollider2 = false;
+	private boolean projCollider1 = false;*/
 	Image land;
-	Image chicken;
+	Image chicken1;
+	Image chicken2;
 	Image fire;
-	int chickX=450;
-	int chickY=400;
-	int VelX=0;
-	int VelY=0;
-	int fireX = -20;
-	int fireY = -20;
-	Boolean X=false;
+	int play1X=450;
+	int play1Y=400;
+	int Vel1X=0;
+	int Vel1Y=0;
+	int play2X=900;
+	int play2Y=400;
+	int Vel2X=0;
+	int Vel2Y=0;
+	int fire1X = 2000;
+	int fire1Y = 2000;
+	int fire2X = 2000;
+	int fire2Y = 2000;
+	Boolean bool1=false;
+	Boolean bool2=false;
 	Velocity player1;
 	Velocity proj1;
+	Velocity player2;
+	Velocity proj2;
 	
 	public Map1(int state) {
 		 this.state = state;
@@ -24,37 +37,69 @@ public class Map1 extends BasicGameState {
 	public void init(GameContainer gc, StateBasedGame sbg)
 			throws SlickException {
 		land = new Image("bg.png");
-		chicken= new Image("chickun.png");
+		chicken1= new Image("chickun.png");
+		chicken2= new Image("chickun.png");
 		fire= new Image("fire.png");
-		player1 = new Velocity(chickX, chickY, VelX, VelY);
+		player1 = new Velocity(play1X, play1Y, Vel1X, Vel1Y);
+		player2 = new Velocity(play2X, play2Y, Vel2X, Vel2Y);
 	}
 
 	@Override
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g)
 			throws SlickException {
 		land.draw(0, 0, gc.getWidth(), gc.getHeight());
-		chicken.draw(chickX, chickY, gc.getWidth()/9, gc.getHeight()/8);
-		fire.draw(fireX, fireY, 25, 25);
+		chicken1.draw(play1X, play1Y, gc.getWidth()/9, gc.getHeight()/8);
+		chicken2.draw(play2X, play2Y, gc.getWidth()/9, gc.getHeight()/8);
+		fire.draw(fire1X, fire1Y);
 	}
 
 	@Override
 	public void update(GameContainer gc, StateBasedGame sbg, int delta)
 			throws SlickException {
 		Input input = gc.getInput();
-		player1.CalcPos(gc);
-		chickX=player1.posX();
-		chickY=player1.posY();
-		if(input.isKeyDown(Input.KEY_I)){
-			X=true;
-			fireX=chickX;
-			fireY=chickY;
-			proj1 = new Velocity(fireX, fireY, 20, -10);
+		
+		//
+		//PLAYER 1
+		player1.CalcPosPlay1(gc);
+		play1X=player1.posX();
+		play1Y=player1.posY();
+		if(input.isKeyDown(Input.KEY_T)){
+			bool1=true;
+			fire1X=play1X;
+			fire1Y=play1Y;
+			System.out.println("fire1Y part 1 is " + fire1Y);
+			System.out.println("play1Y is " + play1Y);
+			proj1 = new Velocity(fire1X+80, fire1Y, 20, -5);
 		}
-		if(X==true){
+		
+		if(bool1==true){
 		proj1.CalcProj();
-		fireX=proj1.posX();
-		fireY=proj1.posY();
+		fire1X=proj1.posX();
+		fire1Y=proj1.posY();
+		System.out.println("fire1Y part 2 is " + fire1Y);
 		}
+		//END OF PLAYER 1
+		//
+		
+		//
+		//PLAYER 2
+		player2.CalcPosPlay2(gc);
+		play2X=player2.posX();
+		play2Y=player2.posY();
+		if(input.isKeyDown(Input.KEY_P)){
+			bool2=true;
+			fire2X=play2X;
+			fire2Y=play2Y;
+			proj2 = new Velocity(fire2X+80, fire2Y, -20, -5);
+		}
+		
+		if(bool2==true){
+		proj2.CalcProj();
+		fire2X=proj2.posX();
+		fire2Y=proj2.posY();
+		}
+		//END OF PLAYER 2
+		//
 	}
 
 	@Override
--- src/Velocity.java
+++ src/Velocity.java
@@ -1,8 +1,6 @@
 import org.newdawn.slick.*;
 
 public class Velocity {
-	int posx;
-	int posy;
 	int chickX;
 	int chickY;
 	int VelX;
@@ -10,11 +8,12 @@ public class Velocity {
 	public Velocity(int chickX2, int chickY2, int velX2,int velY2) 
 	{
 	 chickX=chickX2;
-	 chickX=chickY2;
+	 chickY=chickY2;
 	 VelX=velX2;
 	 VelY=velY2;
 	}
-	public void CalcPos(GameContainer gc){
+	
+	public void CalcPosPlay1(GameContainer gc){
 		Input input = gc.getInput();
 		if(chickY>=520 && chickY<635 && chickX>350 && chickX<1250){
 			chickY=520;
@@ -57,8 +56,8 @@ public class Velocity {
 		chickY=chickY-VelY;
 		chickX=chickX+VelX;
 	}
-	
-	public void CalcProj(){
+	public void CalcPosPlay2(GameContainer gc){
+		Input input2 = gc.getInput();
 		if(chickY>=520 && chickY<635 && chickX>350 && chickX<1250){
 			chickY=520;
 			VelY=0;
@@ -69,21 +68,57 @@ public class Velocity {
 		if(chickY>610 && chickX>600 && chickX<1250){
 			chickX=1250;
 		}
+		if(input2.isKeyDown(Input.KEY_ENTER)){
+			if(VelY<5)
+			VelY=20;
+		}
+		VelY--;
+		if(VelY>20)
+		VelY=20;
+		if(VelY<-20)
+			VelY=-20;
+		if(input2.isKeyDown(Input.KEY_LEFT)){
+		if(VelX>1)
+			VelX=1;
+		VelX-=2;
+		}
+		if(input2.isKeyDown(Input.KEY_RIGHT)){
+			if(VelX<-1)
+				VelX=-1;
+			VelX+=2;
+		}
+		if(VelX<-20)
+			VelX=-20;
+			if(VelX>20)
+				VelX=20;
+		if(VelX>0)
+		VelX--;
+		if(VelX<0)
+		VelX++;
+		
 		chickY=chickY-VelY;
 		chickX=chickX+VelX;
-		
-		if(chickX>1600){
-			chickX=-40;
-			VelX=0;
-			VelY=0;
+	}
+	
+	public void CalcProj(){
+		if(chickY>=599 && chickY<635 && chickX>350 && chickX<1250){
+			chickY=999;
 		}
+		if(chickY>610 && chickX<600 && chickX>200){
+			chickX=200;
+		}
+		if(chickY>610 && chickX>600 && chickX<1250){
+			chickX=1250;
+		}
+		chickY=chickY-VelY;
+		chickX=chickX+VelX;
 	}
 	
 	public int posX(){
 		return chickX;
 	}
+	
 	public int posY(){
 		return chickY;
 	}
-	
 }
